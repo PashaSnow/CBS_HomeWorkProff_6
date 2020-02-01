24d7740acc3543d5044c141053db019e2cefad9c
Pavel Bauman <baumanfromkpi@gmail.com>
2020-02-01 07:49:21 +0200

    Домашка по курсу С# Prof - Рефлексия (Урок 6)

---
+++ Task2/TemperatureConverter.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+
+/// <summary>
+/// Это Task 2
+/// </summary>
+namespace TemperatureConverter
+{
+    public class TemperatureConverter
+    {
+        public double ToCelsius(double farangate)
+        {
+            // если температура ниже абсолютного нуля в фаренгейтах
+            if (farangate < -459.67)
+                farangate = -459.67;
+            return (farangate - 32d) / 1.8d;
+        }
+
+        public double ToFahrenheit(double celsius)
+        {
+            // если температура ниже абсолютного нуля в цельсиях
+            if (celsius < -273.15)
+                celsius = -273.15;
+            return (celsius * 1.8d) + 32;
+        }
+    }
+}
---
+++ Task3/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Reflection;
+using System.IO;
+
+namespace Task3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                Assembly assembly = Assembly.Load("TemperatureConverter");
+                Type myType = assembly.GetType("TemperatureConverter.TemperatureConverter");
+
+                // зададим приблизительные температуры
+                double inCelsius = 25d;
+                double inFahrenheit = 73d;
+
+                dynamic inst = Activator.CreateInstance(myType);
+                Console.WriteLine("В фаренгейтах = {0} - в цельсиях = {1:#.#}", inFahrenheit, inst.ToCelsius(inFahrenheit));
+                Console.WriteLine("В цельсиях = {0} - в фаренгейтах = {1:#.#}", inFahrenheit, inst.ToFahrenheit(inCelsius));
+                Console.ReadKey();
+            }
+            catch (FileNotFoundException ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+        }
+    }
+}
